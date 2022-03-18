cfe0f8e3e9237effe37b9bc96216634aad5ebf40
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2022-03-18 02:59:52 -0700

    feat: add register router

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,33 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  async function registerUser(event) {
+    event.preventDefault();
+    const response = await fetch('https://localhost:1337/api/register', {
+    method: 'POST',  
+    headers: {
+      'Content-Type': 'application/json',
+    },  
+    body: JSON.stringify({
+        name,
+        email,
+        password,
+      }), 
+    })
+
+    const data = await response.json();
+    console.log(data);
+
+  }
+
   return (
     <div>
       <h1>Register</h1>
-      <form style={{display: 'flex', flexDirection: 'column'}}>
+      <form onSubmit={registerUser} style={{display: 'flex', flexDirection: 'column'}}>
         <input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder="Name"></input>
         <input value={email} type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email"></input>
         <input value={password} type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password"></input>
+        <input type="submit" value="Register"></input>
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.get('/', (req,res) =>{
-    res.send("hello world!");
-})
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/api/register', async (req,res) =>{
+    console.log(req.body);
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({statue:'ok'});
+    } catch (error) {
+        res.json({statue:'error'});
+    }
+    res.json({status: 'success'});
+});
+
+// app.post('/api/login', async (req,res) =>{
+//     try {
+   
+//         })
+//         res.json({statue:'ok'});
+//     } catch (error) {
+//         res.json({statue:'error'});
+//     }
+//     res.json({status: 'success'});
+// });
+
+
+
+
 
 app.listen(1337,()=>{
-    console.log("server started!")
-})
+    console.log("server started!");
+});
+
